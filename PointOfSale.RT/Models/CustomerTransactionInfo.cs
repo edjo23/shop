f44ae7360af07d736efa6f258ef4ea88fa80298c
Daniel <edjo23.git@outlook.com>
2013-08-31 13:38:03 +1000

    Stop balance from being displayed on cash sale.

--- PointOfSale.RT/Models/CustomerTransactionInfo.cs
+++ PointOfSale.RT/Models/CustomerTransactionInfo.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shop.Contracts.Entities;
+using Windows.UI.Xaml;
 
 namespace PointOfSale.RT.Models
 {
@@ -13,6 +14,19 @@ namespace PointOfSale.RT.Models
 
         public decimal NewBalance { get; set; }
 
+        public bool IsCash()
+        {
+            return Customer.Name.Equals("Cash", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return IsCash() ? "" : Customer.Name;
+            }
+        }
+
         public string NewBalanceText
         {
             get
@@ -20,6 +34,14 @@ namespace PointOfSale.RT.Models
                 var balanceText = String.Format("{0:C}{1}", Math.Abs(NewBalance), NewBalance < 0 ? " CR" : "");
 
                 return String.Format("Your balance is now {0:C}", balanceText);
+            }
+        }
+
+        public Visibility NewBalanceVisibility
+        {
+            get
+            {
+                return IsCash() ? Visibility.Collapsed : Visibility.Visible;
             }
         }
     }
